c1b1769e628a0f8b82dfa38207a6e76b665b6ce1
Sam Goldman <sam@quixotic.io>
2022-08-30 15:49:25 -0400

    refactor(ct): merge `ProxyAdmin` into `ChugSplashManager`

--- packages/contracts/contracts/ChugSplashManager.sol
+++ packages/contracts/contracts/ChugSplashManager.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.9;
 import { Owned } from "@rari-capital/solmate/src/auth/Owned.sol";
 import { Proxy } from "@eth-optimism/contracts-bedrock/contracts/universal/Proxy.sol";
 import { ChugSplashRegistry } from "./ChugSplashRegistry.sol";
-import { ProxyAdmin } from "./ProxyAdmin.sol";
+import { IProxyAdapter } from "./IProxyAdapter.sol";
 import { ProxyUpdater } from "./ProxyUpdater.sol";
 import { Create2 } from "./libraries/Create2.sol";
 import { MerkleTree } from "./libraries/MerkleTree.sol";
@@ -114,6 +114,12 @@ contract ChugSplashManager is Owned {
     );
 
     /**
+     * @notice "Magic" prefix. When prepended to some arbitrary bytecode and used to create a
+     *         contract, the appended bytecode will be deployed as given.
+     */
+    bytes13 internal constant DEPLOY_CODE_PREFIX = 0x600D380380600D6000396000f3;
+
+    /**
      * @notice Address of the ChugSplashRegistry.
      */
     ChugSplashRegistry public immutable registry;
@@ -122,11 +128,6 @@ contract ChugSplashManager is Owned {
      * @notice Address of the ProxyUpdater.
      */
     address public immutable proxyUpdater;
-
-    /**
-     * @notice The ProxyAdmin contract.
-     */
-    ProxyAdmin public proxyAdmin;
 
     /**
      * @notice Name of the project this contract is managing.
@@ -175,7 +176,6 @@ contract ChugSplashManager is Owned {
         registry = _registry;
         proxyUpdater = _proxyUpdater;
         name = _name;
-        proxyAdmin = new ProxyAdmin{ salt: bytes32(0) }(_registry, _proxyUpdater);
     }
 
     /**
@@ -313,7 +313,7 @@ contract ChugSplashManager is Owned {
             proxy = getProxyByName(_action.target);
             if (proxy.code.length == 0) {
                 bytes32 salt = keccak256(bytes(_action.target));
-                Proxy created = new Proxy{ salt: salt }(address(proxyAdmin));
+                Proxy created = new Proxy{ salt: salt }(address(this));
 
                 // Could happen if insufficient gas is supplied to this transaction, should not
                 // happen otherwise. If there's a situation in which this could happen other than a
@@ -329,13 +329,12 @@ contract ChugSplashManager is Owned {
             proxy = proxies[_action.target];
         }
 
-        // Next, we execute the ChugSplash action by routing the setCode/setStorage call through a
-        // ProxyAdmin, which owns the proxies for this project.
+        // Next, we execute the ChugSplash action by calling setCode/setStorage.
         if (_action.actionType == ChugSplashActionType.SET_CODE) {
-            proxyAdmin.setProxyCode(proxy, proxyType, _action.data);
+            _setProxyCode(proxy, proxyType, _action.data);
         } else {
             (bytes32 key, bytes32 val) = abi.decode(_action.data, (bytes32, bytes32));
-            proxyAdmin.setProxyStorage(proxy, proxyType, key, val);
+            _setProxyStorage(proxy, proxyType, key, val);
         }
 
         // Mark the action as executed and update the total number of executed actions.
@@ -417,5 +416,153 @@ contract ChugSplashManager is Owned {
                 Create2.compute(address(this), keccak256(bytes(_name)), type(Proxy).creationCode)
             )
         );
+    }
+
+    /**
+     * @notice Sets new code for the proxy contract's implementation. Note that this scheme is a bit
+     *         different from the standard proxy scheme where one would typically deploy the code
+     *         separately and then set the implementation address. We're doing it this way because
+     *         it gives us a lot more freedom on the client side.
+     *
+     * @param _proxy     Address of the proxy to upgrade.
+     * @param _proxyType The proxy's type. This is the zero-address for default proxies.
+     * @param _code      Creation bytecode to be deployed.
+     */
+    function _setProxyCode(
+        address payable _proxy,
+        bytes32 _proxyType,
+        bytes memory _code
+    ) internal {
+        // TODO: Add a re-entrancy guard to this function if we move away from using
+        // `DEPLOY_CODE_PREFIX`. There is currently no risk of re-entrancy because the prefix
+        // guarantees that no sub-calls can be made in the implementation contract's constructor. In
+        // the future, we might want to move away from the prefix to add support for constructors
+        // that can run arbitrary creation bytecode. It will then become become necessary to add a
+        // re-entrancy guard to prevent a constructor from calling another contract which in turn
+        // calls back into setCode or setStorage.
+
+        // Get the adapter that corresponds to this proxy type.
+        address adapter = registry.adapters(_proxyType);
+        require(adapter != address(0), "ChugSplashManager: proxy type has no adapter");
+
+        // Get the address of the current implementation for the proxy.
+        address implementation = _getProxyImplementation(_proxy, adapter);
+
+        // If the code hash matches the code hash of the new implementation then we return early.
+        if (keccak256(_code) == _getAccountCodeHash(implementation)) {
+            return;
+        }
+
+        // Create the deploycode by prepending the magic prefix.
+        bytes memory deploycode = abi.encodePacked(DEPLOY_CODE_PREFIX, _code);
+
+        // Deploy the code and set the new implementation address.
+        address newImplementation;
+        assembly {
+            newImplementation := create(0x0, add(deploycode, 0x20), mload(deploycode))
+        }
+
+        // Check that the code was actually deployed correctly. It might be impossible to fail this
+        // check. Should only happen if the contract creation from above runs out of gas but this
+        // parent execution thread does NOT run out of gas. Seems like we should be doing this check
+        // anyway though.
+        require(
+            _getAccountCodeHash(newImplementation) == keccak256(_code),
+            "ProxyUpdater: code was not correctly deployed"
+        );
+
+        // Delegatecall the adapter to upgrade the proxy's implementation contract.
+        _upgradeProxyTo(_proxy, adapter, implementation);
+    }
+
+    /**
+     * @notice Modifies a storage slot within the proxy contract.
+     *
+     * @param _proxy     Address of the proxy to upgrade.
+     * @param _proxyType The proxy's type. This is the zero-address for default proxies.
+     * @param _key       Storage key to modify.
+     * @param _value     New value for the storage key.
+     */
+    function _setProxyStorage(
+        address payable _proxy,
+        bytes32 _proxyType,
+        bytes32 _key,
+        bytes32 _value
+    ) internal {
+        // Get the adapter that corresponds to this proxy type.
+        address adapter = registry.adapters(_proxyType);
+        require(adapter != address(0), "ChugSplashManager: proxy type has no adapter");
+
+        // Get the address of the current implementation for the proxy. The ChugSplashManager will
+        // set the proxy's implementation back to this address after setting it to be the
+        // ProxyUpdater and calling `setStorage`.
+        address implementation = _getProxyImplementation(_proxy, adapter);
+
+        // Delegatecall the adapter to upgrade the proxy's implementation to be the ProxyUpdater,
+        // which has the `setStorage` function.
+        _upgradeProxyTo(_proxy, adapter, proxyUpdater);
+
+        // Call the `setStorage` action on the proxy.
+        (bool success, ) = _proxy.call(abi.encodeCall(ProxyUpdater.setStorage, (_key, _value)));
+        require(success, "ChugSplashManager: call to set proxy storage failed");
+
+        // Delegatecall the adapter to set the proxy's implementation back to its original address.
+        _upgradeProxyTo(_proxy, adapter, implementation);
+    }
+
+    /**
+     * @notice Delegatecalls an adapter to get the address of the proxy's implementation contract.
+     *
+     * @param _proxy   Address of the proxy.
+     * @param _adapter Address of the adapter to use for the proxy.
+     */
+    function _getProxyImplementation(address payable _proxy, address _adapter)
+        internal
+        returns (address)
+    {
+        (bool success, bytes memory implementationBytes) = _adapter.delegatecall(
+            abi.encodeCall(IProxyAdapter.getProxyImplementation, (_proxy))
+        );
+        require(success, "ChugSplashManager: delegatecall to get proxy implementation failed");
+
+        // Convert the implementation's type from bytes to address.
+        address implementation;
+        assembly {
+            implementation := mload(add(implementationBytes, 32))
+        }
+        return implementation;
+    }
+
+    /**
+     * @notice Delegatecalls an adapter to upgrade a proxy's implementation contract.
+     *
+     * @param _proxy          Address of the proxy to upgrade.
+     * @param _adapter        Address of the adapter to use for the proxy.
+     * @param _implementation Address to set as the proxy's new implementation contract.
+     */
+    function _upgradeProxyTo(
+        address payable _proxy,
+        address _adapter,
+        address _implementation
+    ) internal {
+        (bool success, ) = _adapter.delegatecall(
+            abi.encodeCall(IProxyAdapter.upgradeProxyTo, (_proxy, _implementation))
+        );
+        require(success, "ChugSplashManager: delegatecall to upgrade proxy failed");
+    }
+
+    /**
+     * @notice Gets the code hash for a given account.
+     *
+     * @param _account Address of the account to get a code hash for.
+     *
+     * @return Code hash for the account.
+     */
+    function _getAccountCodeHash(address _account) internal view returns (bytes32) {
+        bytes32 codeHash;
+        assembly {
+            codeHash := extcodehash(_account)
+        }
+        return codeHash;
     }
 }
