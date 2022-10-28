7f1c8e9dd90892a4890c68139a096a221e398051
Sam Goldman <sam@quixotic.io>
2022-10-28 16:18:30 -0400

    fix(ct): remove ownership requirement to propose bundles

---
+++ .changeset/breezy-readers-deliver.md
@@ -0,0 +1,5 @@
+---
+'@chugsplash/contracts': patch
+---
+
+Remove ownership requirement to propose bundles
--- packages/demo/hardhat.config.ts
+++ packages/demo/hardhat.config.ts
@@ -8,8 +8,6 @@ import '@chugsplash/plugins'
 // Load environment variables from .env
 dotenv.config()
 
-const PRIVATE_KEY: any = process.env.PRIVATE_KEY
-
 const config: HardhatUserConfig = {
   solidity: {
     version: '0.8.15',
@@ -24,11 +22,6 @@ const config: HardhatUserConfig = {
   networks: {
     localhost: {
       url: 'http://localhost:8545',
-    },
-    goerli: {
-      chainId: 5,
-      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
     },
   },
 }
