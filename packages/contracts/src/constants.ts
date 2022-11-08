a536675219a3c099b3e91f5f6746f7e045357b71
Sam Goldman <sam@quixotic.io>
2022-11-08 13:52:40 -0500

    fix(ct): export constructor args for chugsplash predeploys

--- packages/contracts/src/constants.ts
+++ packages/contracts/src/constants.ts
@@ -6,7 +6,29 @@ import {
   ProxyUpdaterArtifact,
   // ChugSplashRegistryArtifact,
   ChugSplashBootLoaderArtifact,
+  ChugSplashRegistryArtifact,
+  ChugSplashManagerArtifact,
+  ChugSplashManagerProxyArtifact,
+  ChugSplashManagerABI,
+  ProxyABI,
 } from './ifaces'
+
+const owner = '0x1A3DAA6F487A480c1aD312b90FD0244871940b66'
+
+const chugsplashRegistrySourceName = ChugSplashRegistryArtifact.sourceName
+const chugsplashBootLoaderSourceName = ChugSplashBootLoaderArtifact.sourceName
+const chugsplashManagerProxySourceName =
+  ChugSplashManagerProxyArtifact.sourceName
+const chugsplashManagerSourceName = ChugSplashManagerArtifact.sourceName
+const proxyUpdaterSourceName = ProxyUpdaterArtifact.sourceName
+const defaultAdapterSourceName = DefaultAdapterArtifact.sourceName
+const chugsplashRegistyProxySourceName = ProxyArtifact.sourceName
+
+const [chugsplashManagerConstructorFragment] = ChugSplashManagerABI.filter(
+  (fragment) => fragment.type === 'constructor'
+)
+const chugsplashManagerConstructorArgTypes =
+  chugsplashManagerConstructorFragment.inputs.map((input) => input.type)
 
 export const DETERMINISTIC_DEPLOYMENT_PROXY_ADDRESS =
   '0x4e59b44847b379578588920ca78fbf26c0b4956c'
@@ -31,6 +53,13 @@ export const PROXY_UPDATER_ADDRESS = ethers.utils.getCreate2Address(
   ethers.utils.solidityKeccak256(['bytes'], [ProxyUpdaterArtifact.bytecode])
 )
 
+const [registryProxyConstructorFragment] = ProxyABI.filter(
+  (fragment) => fragment.type === 'constructor'
+)
+const registryProxyConstructorArgTypes =
+  registryProxyConstructorFragment.inputs.map((input) => input.type)
+const registryProxyConstructorArgValues = [CHUGSPLASH_BOOTLOADER_ADDRESS]
+
 export const CHUGSPLASH_REGISTRY_PROXY_ADDRESS = ethers.utils.getCreate2Address(
   CHUGSPLASH_BOOTLOADER_ADDRESS,
   ethers.constants.HashZero,
@@ -39,8 +68,8 @@ export const CHUGSPLASH_REGISTRY_PROXY_ADDRESS = ethers.utils.getCreate2Address(
     [
       ProxyArtifact.bytecode,
       ethers.utils.defaultAbiCoder.encode(
-        ['address'],
-        [CHUGSPLASH_BOOTLOADER_ADDRESS]
+        registryProxyConstructorArgTypes,
+        registryProxyConstructorArgValues
       ),
     ]
   )
@@ -90,3 +119,50 @@ export const DEFAULT_ADAPTER_ADDRESS = ethers.utils.getCreate2Address(
   ethers.utils.solidityKeccak256(['string'], ['DefaultAdapter']),
   ethers.utils.solidityKeccak256(['bytes'], [DefaultAdapterArtifact.bytecode])
 )
+
+const chugsplashManagerConstructorArgValues = [
+  CHUGSPLASH_REGISTRY_PROXY_ADDRESS,
+  'Root Manager',
+  owner,
+  PROXY_UPDATER_ADDRESS,
+  EXECUTOR_BOND_AMOUNT,
+  EXECUTION_LOCK_TIME,
+  OWNER_BOND_AMOUNT,
+  EXECUTOR_PAYMENT_PERCENTAGE,
+]
+
+export const CHUGSPLASH_MANAGER_ADDRESS = ethers.utils.getCreate2Address(
+  DETERMINISTIC_DEPLOYMENT_PROXY_ADDRESS,
+  ethers.constants.HashZero,
+  ethers.utils.solidityKeccak256(
+    ['bytes', 'bytes'],
+    [
+      ChugSplashManagerArtifact.bytecode,
+      ethers.utils.defaultAbiCoder.encode(
+        chugsplashManagerConstructorArgTypes,
+        chugsplashManagerConstructorArgValues
+      ),
+    ]
+  )
+)
+
+export const CHUGSPLASH_CONSTRUCTOR_ARGS = {}
+CHUGSPLASH_CONSTRUCTOR_ARGS[chugsplashRegistrySourceName] = [
+  PROXY_UPDATER_ADDRESS,
+  OWNER_BOND_AMOUNT,
+  EXECUTOR_BOND_AMOUNT,
+  EXECUTION_LOCK_TIME,
+  EXECUTOR_PAYMENT_PERCENTAGE,
+  CHUGSPLASH_MANAGER_ADDRESS,
+]
+CHUGSPLASH_CONSTRUCTOR_ARGS[chugsplashBootLoaderSourceName] = []
+CHUGSPLASH_CONSTRUCTOR_ARGS[chugsplashManagerProxySourceName] = [
+  CHUGSPLASH_REGISTRY_PROXY_ADDRESS,
+  CHUGSPLASH_BOOTLOADER_ADDRESS,
+]
+CHUGSPLASH_CONSTRUCTOR_ARGS[chugsplashManagerSourceName] =
+  chugsplashManagerConstructorArgValues
+CHUGSPLASH_CONSTRUCTOR_ARGS[proxyUpdaterSourceName] = []
+CHUGSPLASH_CONSTRUCTOR_ARGS[defaultAdapterSourceName] = []
+CHUGSPLASH_CONSTRUCTOR_ARGS[chugsplashRegistyProxySourceName] =
+  registryProxyConstructorArgValues
