693ca0f66c9333fe175da4a03441d31f3c80b5e2
Sam Goldman <sam@quixotic.io>
2022-12-09 15:20:17 -0500

    fix(pg): remove hard-coded proxy address in config

---
+++ .changeset/spotty-chefs-buy.md
@@ -0,0 +1,5 @@
+---
+'@chugsplash/plugins': patch
+---
+
+Removed hard-coded proxy address
--- packages/plugins/chugsplash/Storage.config.ts
+++ packages/plugins/chugsplash/Storage.config.ts
@@ -10,7 +10,6 @@ const config: UserChugSplashConfig = {
   contracts: {
     Storage: {
       contract: 'Storage',
-      proxy: '0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6',
       variables,
     },
   },
