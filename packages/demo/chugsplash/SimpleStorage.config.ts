ad6a5be86b0df28dc60bb0b5f877b5787776bd8e
Sam Goldman <sam@quixotic.io>
2022-12-12 16:58:16 -0500

    fix: update plugins readme and demo

--- packages/demo/chugsplash/SimpleStorage.config.ts
+++ packages/demo/chugsplash/SimpleStorage.config.ts
@@ -1,33 +1,21 @@
-import { UserChugSplashConfig } from '@chugsplash/core'
+import { ChugSplashConfig } from '@chugsplash/core'
 
-const config: UserChugSplashConfig = {
+const config: ChugSplashConfig = {
   // Configuration options for the project:
   options: {
     projectName: 'My First Project',
   },
   // Below, we define all of the contracts in the deployment along with their state variables.
   contracts: {
-    // First contract config:
     FirstSimpleStorage: {
       contract: 'SimpleStorage',
       variables: {
         number: 1,
         stored: true,
         storageName: 'First',
-        otherStorage: { '!Ref': 'SecondSimpleStorage' }, // Reference to SecondSimpleStorage
-      },
-    },
-    // Second contract config:
-    SecondSimpleStorage: {
-      contract: 'SimpleStorage',
-      variables: {
-        number: 2,
-        stored: true,
-        storageName: 'Second',
         otherStorage: '0x1111111111111111111111111111111111111111',
       },
     },
   },
 }
-
 export default config
--- packages/demo/test/SimpleStorage.spec.ts
+++ packages/demo/test/SimpleStorage.spec.ts
@@ -3,21 +3,19 @@ import { Contract } from 'ethers'
 
 describe('SimpleStorage', () => {
   let FirstSimpleStorage: Contract
-  let SecondSimpleStorage: Contract
   beforeEach(async () => {
     // You must reset your ChugSplash deployments to their initial state here
     await chugsplash.reset()
 
     FirstSimpleStorage = await chugsplash.getContract('FirstSimpleStorage')
-    SecondSimpleStorage = await chugsplash.getContract('SecondSimpleStorage')
   })
 
   it('initializes correctly', async () => {
-    expect(await FirstSimpleStorage.getNumber()).equals(1)
-    expect(await FirstSimpleStorage.getOtherStorage()).equals(
-      SecondSimpleStorage.address
+    expect(await FirstSimpleStorage.number()).equals(1)
+    expect(await FirstSimpleStorage.stored()).equals(true)
+    expect(await FirstSimpleStorage.storageName()).equals('First')
+    expect(await FirstSimpleStorage.otherStorage()).equals(
+      '0x1111111111111111111111111111111111111111'
     )
-    expect(await SecondSimpleStorage.isStored()).equals(true)
-    expect(await SecondSimpleStorage.getStorageName()).equals('Second')
   })
 })
