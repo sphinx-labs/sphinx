ef570a37c3a4815175354c716348f84ab5010197
Sam Goldman <sam@quixotic.io>
2023-01-28 17:10:45 -0500

    wip

--- packages/demo/contracts/HelloChugSplash.sol
+++ packages/demo/contracts/HelloChugSplash.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.15;
 
 contract HelloChugSplash {
-    uint8 public number;
+    uint8 public immutable number = 123;
     bool public stored;
     address public otherStorage;
     string public storageName;
