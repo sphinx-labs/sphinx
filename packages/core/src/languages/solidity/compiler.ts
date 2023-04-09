5896c7c18d01a44b1add271d24f96246318e4bad
Sam Goldman <sam@quixotic.io>
2023-04-09 13:10:37 -0700

    fix(core): remove unnecessary `getMinimumSourceNames` function

--- packages/core/src/languages/solidity/compiler.ts
+++ packages/core/src/languages/solidity/compiler.ts
@@ -8,11 +8,7 @@ import { providers } from 'ethers'
 
 import { CanonicalChugSplashConfig } from '../../config/types'
 import { ChugSplashBundles, makeBundlesFromConfig } from '../../actions'
-import {
-  CompilerOutputContracts,
-  CompilerOutputMetadata,
-  CompilerOutputSources,
-} from './types'
+import { CompilerOutputContracts, CompilerOutputMetadata } from './types'
 import { getCanonicalConfigArtifacts } from '../../utils'
 
 export const bundleRemoteSubtask = async (args: {
@@ -107,49 +103,4 @@ export const getMinimumCompilerInput = (
   }
 
   return minimumCompilerInput
-}
-
-/**
- * Recursively get the minimum list of source names necessary to compile a given source name. All
- * source names that are referenced in the given source name must be included in this list.
- *
- * @param sourceName The source name.
- * @param fullOutputSources The full compiler output source object.
- * @param contractAstIdsToSourceNames Mapping from contract AST IDs to source names.
- * @param minimumSourceNames Array of minimum source names.
- * @returns
- */
-export const getMinimumSourceNames = (
-  sourceName: string,
-  fullOutputSources: CompilerOutputSources,
-  contractAstIdsToSourceNames: { [astId: number]: string },
-  minimumSourceNames: string[]
-): string[] => {
-  // The exported symbols object contains the AST IDs corresponding to the contracts that must be
-  // included in the list of minimum source names for the given source.
-  const exportedSymbols = fullOutputSources[sourceName].ast.exportedSymbols
-
-  if (exportedSymbols) {
-    for (const astIds of Object.values(exportedSymbols)) {
-      if (astIds === undefined) {
-        continue
-      } else if (astIds.length > 1) {
-        throw new Error(
-          `Detected more than one AST ID for: ${sourceName}. Please report this error.`
-        )
-      }
-      const astId = astIds[0]
-      const nextSourceName = contractAstIdsToSourceNames[astId]
-      if (!minimumSourceNames.includes(nextSourceName)) {
-        minimumSourceNames.push(nextSourceName)
-        minimumSourceNames = getMinimumSourceNames(
-          nextSourceName,
-          fullOutputSources,
-          contractAstIdsToSourceNames,
-          minimumSourceNames
-        )
-      }
-    }
-  }
-  return minimumSourceNames
 }
