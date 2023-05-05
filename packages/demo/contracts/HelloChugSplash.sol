2a11da77259f9a5dc2c382a975edabfb826e7200
Sam Goldman <samgoldman12@gmail.com>
2023-05-05 02:36:23 -0400

    demo non-proxy contract

--- packages/demo/contracts/HelloChugSplash.sol
+++ packages/demo/contracts/HelloChugSplash.sol
@@ -6,4 +6,11 @@ contract HelloChugSplash {
     bool public stored;
     address public otherStorage;
     string public storageName;
+
+    constructor(uint8 _number, bool _stored, address _otherStorage, string memory _storageName) {
+        number = _number;
+        stored = _stored;
+        otherStorage = _otherStorage;
+        storageName = _storageName;
+    }
 }
