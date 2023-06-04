7f01626c5b817b338e0693a5ee6ba353479bd287
Sam Goldman <samgoldman12@gmail.com>
2023-06-04 12:12:40 -0400

    a

--- packages/plugins/chugsplash/foundry/claim.t.js
+++ packages/plugins/chugsplash/foundry/claim.t.js
@@ -2,68 +2,30 @@ const { ethers } = require('ethers')
 
 const projectName = 'Claim test'
 
-const hre = require('hardhat')
-require('@nomiclabs/hardhat-ethers')
-
-
-const main = async () => {
-  const provider = hre.ethers.provider
-  const data = await provider.getNetwork()
-
-  return {
-    // Configuration options for the project:
-    options: {
-      organizationID: ethers.utils.keccak256(
-        ethers.utils.toUtf8Bytes(projectName)
-      ),
-      projectName,
-    },
-    contracts: {
-      MySimpleStorage: {
-        contract: 'SimpleStorage',
-        variables: {
-          myStorage: '0x1111111111111111111111111111111111111111',
-          myStateless: '0x1111111111111111111111111111111111111111',
-        },
-        constructorArgs: {
-          _immutableContractReference:
-            '0x1111111111111111111111111111111111111111',
-          _statelessImmutableContractReference:
-            '0x1111111111111111111111111111111111111111',
-        },
+module.exports = {
+  // Configuration options for the project:
+  options: {
+    organizationID: ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes(projectName)
+    ),
+    projectName,
+  },
+  contracts: {
+    MySimpleStorage: {
+      contract: 'SimpleStorage',
+      variables: {
+        myStorage: '0x1111111111111111111111111111111111111111',
+        myStateless: '0x1111111111111111111111111111111111111111',
+      },
+      constructorArgs: {
+        // _immutableContractReference:
+          // '0x1111111111111111111111111111111111111111',
+        _statelessImmutableContractReference:
+          '0x1111111111111111111111111111111111111111',
+      },
+      unsafeAllow: {
+        delegatecall: true,
       },
     },
-  }
+  },
 }
-
-const result = main()
-
-module.exports = result
-
-// module.exports = {
-//   // Configuration options for the project:
-//   options: {
-//     organizationID: ethers.utils.keccak256(
-//       ethers.utils.toUtf8Bytes(projectName)
-//     ),
-//     projectName,
-//   },
-//   contracts: {
-//     MySimpleStorage: {
-//       contract: 'SimpleStorage',
-//       variables: {
-//         myStorage: '0x1111111111111111111111111111111111111111',
-//         myStateless: '0x1111111111111111111111111111111111111111',
-//       },
-//       constructorArgs: {
-//         // _immutableContractReference:
-//         //   '0x1111111111111111111111111111111111111111',
-//         _statelessImmutableContractReference:
-//           '0x1111111111111111111111111111111111111111',
-//       },
-//       unsafeAllow: {
-//         delegatecall: true,
-//       }
-//     },
-//   },
-// }
