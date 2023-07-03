846551112cc3a323238c091d2b7b4d2bc979ef44
Sam Goldman <samgoldman12@gmail.com>
2023-07-03 02:18:57 -0400

    a

---
+++ packages/plugins/test/run-hh-tests.sh
@@ -0,0 +1,2 @@
+npx hardhat test test/main/* --config-path chugsplash/main.config.ts --projects 'Create3, Storage' &&
+npx hardhat test test/ManagerUpgrade.spec.ts --config-path chugsplash/manager-upgrade.config.ts
