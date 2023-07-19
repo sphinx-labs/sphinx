7a062b581219ca31ef6a4c5c2c82f63bd4962cf9
Ryan Pate <ryantpate97@gmail.com>
2023-07-18 17:25:16 -0700

    fix(pg): Update plugins package network endpoints

--- packages/plugins/hardhat.config.ts
+++ packages/plugins/hardhat.config.ts
@@ -35,32 +35,32 @@ const config: HardhatUserConfig = {
   networks: {
     goerli: {
       chainId: 5,
-      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: 'http://localhost:42005',
       accounts,
     },
     ethereum: {
       chainId: 1,
-      url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: 'http://localhost:42001',
       accounts,
     },
     'optimism-goerli': {
       chainId: 420,
-      url: `https://opt-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: 'http://localhost:42420',
       accounts,
     },
     optimism: {
       chainId: 10,
-      url: `https://opt-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: 'http://localhost:42010',
       accounts,
     },
     arbitrum: {
       chainId: 42161,
-      url: 'https://arb1.arbitrum.io/rpc',
+      url: 'http://localhost:42161',
       accounts,
     },
     'arbitrum-goerli': {
       chainId: 421613,
-      url: `https://arb-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: 'http://localhost:42613',
       accounts,
     },
   },
