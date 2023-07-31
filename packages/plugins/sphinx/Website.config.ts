e9fc6c67be791ad6a81798d0318ebd01014bef38
Ryan Pate <ryantpate97@gmail.com>
2023-07-31 13:46:59 -0700

    feat(core): output warning for unreliable networks

--- packages/plugins/sphinx/Website.config.ts
+++ packages/plugins/sphinx/Website.config.ts
@@ -4,9 +4,9 @@ const ownerAddress = '0x9fd58Bf0F2E6125Ffb0CBFa9AE91893Dbc1D5c51'
 
 // Used for testing the website, please do not delete
 const config: UserConfigWithOptions = {
-  projectName: 'Multichain Sphinx Deployment',
+  projectName: 'Foundry Deployment 3',
   options: {
-    orgId: 'clkom3nto0000olcbakxf23p1',
+    orgId: 'clkr8uf810000gecb3w80g8cl',
     owners: [ownerAddress],
     threshold: 1,
     testnets: [
