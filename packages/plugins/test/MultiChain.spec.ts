0b309f0c592020a2fb072c6b9e30632811eb1d87
Sam Goldman <samgoldman12@gmail.com>
2023-08-07 23:27:15 -0700

    feat(pg): Add dry-run and confirm flags to the propose task

--- packages/plugins/test/MultiChain.spec.ts
+++ packages/plugins/test/MultiChain.spec.ts
@@ -168,12 +168,12 @@ describe('Multi chain config', () => {
         newUserConfig,
         true,
         cre,
+        true, // Dry run the proposal so it isn't sent to the back-end
         makeGetConfigArtifacts(hre),
         makeGetProviderFromChainId(hre),
         undefined, // Use the default spinner
         undefined, // Use the default FailureAction
-        getCanonicalConfig,
-        true // Skip relaying the meta transaction to the back-end
+        getCanonicalConfig
       )
       const { root, leaves } = proposalRequest.tree
 
