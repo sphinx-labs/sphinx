729ede37c153d9c689b1dacc22b462c683763aa4
Sam Goldman <samgoldman12@gmail.com>
2023-09-05 16:41:10 -0400

    a

---
+++ packages/core/src/validate.ts
@@ -0,0 +1,324 @@
+import { ethers } from 'ethers'
+import { SphinxManagerABI } from '@sphinx-labs/contracts'
+import { decodeAllSync } from 'cbor'
+
+import {
+  ConfigArtifacts,
+  ConfigCache,
+  ParsedConfig,
+  getCreate3Address,
+} from './config'
+import { SphinxJsonRpcProvider } from './provider'
+import {
+  SphinxActionBundle,
+  fromRawSphinxAction,
+  isDeployContractAction,
+} from './actions'
+import { CallFrame } from './languages/solidity/types'
+import {
+  callActionWasExecuted,
+  getCallHash,
+  getEncodedConstructorArgs,
+  isSupportedChainId,
+  remove0x,
+} from './utils'
+
+// TODO(docs): we prefer `debug_traceTransaction` to `trace_transaction` because it's supported by
+// more execution clients (e.g. geth)
+
+export const flattenCallFrames = (
+  rootCallFrame: CallFrame
+): Array<CallFrame> => {
+  const callFrames: Array<CallFrame> = []
+
+  const flatten = (child: CallFrame): void => {
+    callFrames.push(child)
+
+    for (const childCallFrame of child.calls) {
+      flatten(childCallFrame)
+    }
+  }
+
+  flatten(rootCallFrame)
+
+  return callFrames
+}
+
+// TODO(docs): everywhere
+export const validate = async (
+  provider: SphinxJsonRpcProvider,
+  parsedConfig: ParsedConfig,
+  actionBundle: SphinxActionBundle,
+  configArtifacts: ConfigArtifacts,
+  configCache: ConfigCache
+): void => {
+  try {
+    await provider.send('trace_transaction', [ethers.ZeroHash])
+  } catch (e) {
+    // TODO: handle error
+  }
+
+  const deploymentActions = actionBundle.actions
+    .map((rawAction) => fromRawSphinxAction(rawAction.action))
+    .filter(isDeployContractAction)
+
+  const sphinxManagerAddress = parsedConfig.manager
+  const SphinxManager = new ethers.Contract(
+    sphinxManagerAddress,
+    SphinxManagerABI,
+    provider
+  )
+
+  const chainId = configCache.chainId
+
+  if (!isSupportedChainId(chainId)) {
+    throw new Error(`TODO: should never happen`)
+  }
+
+  for (const [referenceName, contractConfig] of Object.entries(
+    parsedConfig.contracts
+  )) {
+    const { isTargetDeployed } = configCache.contractConfigCache[referenceName]
+
+    if (!isTargetDeployed) {
+      // TODO: handle this
+      continue
+    }
+
+    const expectedAddress = contractConfig[referenceName].address
+    const action = deploymentActions.find(
+      (_action) =>
+        expectedAddress ===
+        getCreate3Address(sphinxManagerAddress, _action.salt)
+    )
+
+    if (!action) {
+      // TODO: throw error, should never happen.
+      throw new Error(`TODO`)
+    }
+
+    const deploymentEvents = await SphinxManager.queryFilter(
+      SphinxManager.filters.ContractDeployed(expectedAddress)
+    )
+    if (deploymentEvents.length === 0) {
+      // TODO: handle this
+    } else if (deploymentEvents.length > 1) {
+      // TODO: handle this
+    }
+    const { transactionHash } = deploymentEvents[0]
+
+    const receipt = await provider.getTransactionReceipt(transactionHash)
+    if (!receipt) {
+      // TODO: handle this. should never happen b/c we got the txn hash from an event
+    } else if (receipt.status === 0) {
+      // TODO: handle this. 'status = 0' means the transaction reverted. should never happen b/c the
+      // presence of the 'ContractDeployed' event means the transaction should have succeeded.
+    }
+
+    const callFrames: CallFrame = await provider.send(
+      'debug_traceTransaction',
+      [transactionHash, { tracer: 'callTracer' }]
+    )
+
+    const flattenedCallFrames = flattenCallFrames(callFrames)
+
+    const deploymentCallFrameExactMatch = flattenedCallFrames.find(
+      (callFrame: CallFrame): boolean =>
+        callFrame.input === action.creationCodeWithConstructorArgs &&
+        callFrame.to === expectedAddress &&
+        callFrame.type === 'CREATE'
+    )
+
+    if (deploymentCallFrameExactMatch) {
+      // TODO: exact match :)
+    } else {
+      // TODO(docs): couldn't find an exact match. the most likely reason is that the contract's
+      // metadata hash is slightly different on the user's local machine than it was when the
+      // contract was deployed. a different metadata hash doesn't impact the behavior of the
+      // contract, but it does mean that there is a mismatch in the compiler inputs (e.g. compiler
+      // settings, natspec docs, source files, etc.). for a detailed description of the metadata,
+      // see here: https://docs.soliditylang.org/en/latest/metadata.html
+
+      // TODO(docs): we proceed by checking if there's a contract that matches the contract's
+      // creation code and constructor args, excluding the metadata hash.
+
+      const expectedCreationCodeWithoutConstructorArgs =
+        configArtifacts[referenceName].artifact.bytecode
+
+      const expectedConstructorArgs = remove0x(
+        getEncodedConstructorArgs(
+          contractConfig[referenceName].constructorArgs,
+          configArtifacts[referenceName].artifact.abi
+        )
+      )
+
+      // TODO(docs): get the last two bytes of the expected creation code without constructor args
+      const encodedMetadataLengthHex = ethers.dataSlice(
+        expectedCreationCodeWithoutConstructorArgs,
+        -2
+      )
+      const encodedMetadataLength = Number(encodedMetadataLengthHex)
+      const expectedCreationCodeWithoutMetadataHash = ethers.dataSlice(
+        expectedCreationCodeWithoutConstructorArgs,
+        -(encodedMetadataLength + 2),
+        -2
+      )
+
+      const deploymentCallFrameSimilarMatch = flattenedCallFrames.find(
+        (callFrame: CallFrame): boolean =>
+          callFrame.input.startsWith(expectedCreationCodeWithoutMetadataHash) &&
+          callFrame.input.endsWith(expectedConstructorArgs) &&
+          callFrame.to === expectedAddress &&
+          callFrame.type === 'CREATE'
+      )
+
+      if (!deploymentCallFrameSimilarMatch) {
+        // TODO: handle this. could be b/c of local differences in the contract's source code, or it
+        // could be b/c there's an embedded metadata hash that's different (if the contract deploys
+        // another contract), or it could be b/c our system has a bug
+        continue
+      }
+
+      // TODO(docs): Sanity check that a valid metadata hash exists at the end of the creation code
+      // and before the encoded constructor arguments.
+      const actualCreationCode = deploymentCallFrameSimilarMatch.input
+      const encodedMetadata = actualCreationCode
+        // TODO(docs): remove the constructor args from the end of the actual creation code
+        .slice(0, -expectedConstructorArgs.length)
+        // TODO(docs): remove the contract's creation bytecode from the beginning of the actual
+        // creation code
+        .slice(expectedCreationCodeWithoutConstructorArgs.length)
+
+      const decodedMetadata = decodeAllSync(encodedMetadata)
+
+      if (decodedMetadata.length === 0) {
+        // TODO: handle this case. should never happen.
+      }
+
+      // TODO: handle similar match here
+    }
+  }
+
+  const postDeployActions = parsedConfig.postDeploy[chainId] ?? []
+  const previousActionTimestamp = 0 // TODO
+  for (const action of postDeployActions) {
+    if (
+      !callActionWasExecuted(
+        action.to,
+        action.data,
+        action.nonce,
+        configCache.callNonces
+      )
+    ) {
+      // TODO: handle this
+    }
+
+    const callHash = getCallHash(action.to, action.data)
+    const callEvents = await SphinxManager.queryFilter(
+      SphinxManager.filters.CallExecuted(undefined, callHash)
+    )
+
+    // TODO: rm
+
+    // const deploymentEvents = await SphinxManager.queryFilter(
+    //   SphinxManager.filters.ContractDeployed(expectedAddress)
+    // )
+    // if (deploymentEvents.length === 0) {
+    //   // TODO: handle this
+    // } else if (deploymentEvents.length > 1) {
+    //   // TODO: handle this
+    // }
+    // const { transactionHash } = deploymentEvents[0]
+
+    // const receipt = await provider.getTransactionReceipt(transactionHash)
+    // if (!receipt) {
+    //   // TODO: handle this. should never happen b/c we got the txn hash from an event
+    // } else if (receipt.status === 0) {
+    //   // TODO: handle this. 'status = 0' means the transaction reverted. should never happen b/c the
+    //   // presence of the 'ContractDeployed' event means the transaction should have succeeded.
+    // }
+
+    // const callFrames: CallFrame = await provider.send(
+    //   'debug_traceTransaction',
+    //   [transactionHash, { tracer: 'callTracer' }]
+    // )
+
+    // const flattenedCallFrames = flattenCallFrames(callFrames)
+
+    // const deploymentCallFrameExactMatch = flattenedCallFrames.find(
+    //   (callFrame: CallFrame): boolean =>
+    //     callFrame.input === action.creationCodeWithConstructorArgs &&
+    //     callFrame.to === expectedAddress &&
+    //     callFrame.type === 'CREATE'
+    // )
+
+    // if (deploymentCallFrameExactMatch) {
+    //   // TODO: exact match :)
+    // } else {
+    //   // TODO(docs): couldn't find an exact match. the most likely reason is that the contract's
+    //   // metadata hash is slightly different on the user's local machine than it was when the
+    //   // contract was deployed. a different metadata hash doesn't impact the behavior of the
+    //   // contract, but it does mean that there is a mismatch in the compiler inputs (e.g. compiler
+    //   // settings, natspec docs, source files, etc.). for a detailed description of the metadata,
+    //   // see here: https://docs.soliditylang.org/en/latest/metadata.html
+
+    //   // TODO(docs): we proceed by checking if there's a contract that matches the contract's
+    //   // creation code and constructor args, excluding the metadata hash.
+
+    //   const expectedCreationCodeWithoutConstructorArgs =
+    //     configArtifacts[referenceName].artifact.bytecode
+
+    //   const expectedConstructorArgs = remove0x(
+    //     getEncodedConstructorArgs(
+    //       contractConfig[referenceName].constructorArgs,
+    //       configArtifacts[referenceName].artifact.abi
+    //     )
+    //   )
+
+    //   // TODO(docs): get the last two bytes of the expected creation code without constructor args
+    //   const encodedMetadataLengthHex = ethers.dataSlice(
+    //     expectedCreationCodeWithoutConstructorArgs,
+    //     -2
+    //   )
+    //   const encodedMetadataLength = Number(encodedMetadataLengthHex)
+    //   const expectedCreationCodeWithoutMetadataHash = ethers.dataSlice(
+    //     expectedCreationCodeWithoutConstructorArgs,
+    //     -(encodedMetadataLength + 2),
+    //     -2
+    //   )
+
+    //   const deploymentCallFrameSimilarMatch = flattenedCallFrames.find(
+    //     (callFrame: CallFrame): boolean =>
+    //       callFrame.input.startsWith(expectedCreationCodeWithoutMetadataHash) &&
+    //       callFrame.input.endsWith(expectedConstructorArgs) &&
+    //       callFrame.to === expectedAddress &&
+    //       callFrame.type === 'CREATE'
+    //   )
+
+    //   if (!deploymentCallFrameSimilarMatch) {
+    //     // TODO: handle this. could be b/c of local differences in the contract's source code, or it
+    //     // could be b/c there's an embedded metadata hash that's different (if the contract deploys
+    //     // another contract), or it could be b/c our system has a bug
+    //     continue
+    //   }
+
+    //   // TODO(docs): Sanity check that a valid metadata hash exists at the end of the creation code
+    //   // and before the encoded constructor arguments.
+    //   const actualCreationCode = deploymentCallFrameSimilarMatch.input
+    //   const encodedMetadata = actualCreationCode
+    //     // TODO(docs): remove the constructor args from the end of the actual creation code
+    //     .slice(0, -expectedConstructorArgs.length)
+    //     // TODO(docs): remove the contract's creation bytecode from the beginning of the actual
+    //     // creation code
+    //     .slice(expectedCreationCodeWithoutConstructorArgs.length)
+
+    //   const decodedMetadata = decodeAllSync(encodedMetadata)
+
+    //   if (decodedMetadata.length === 0) {
+    //     // TODO: handle this case. should never happen.
+    //   }
+
+    //   // TODO: handle similar match here
+    // }
+  }
+}
