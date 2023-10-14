0da73ed4b1901062211f4609a099a55dcbe17f8d
Sam Goldman <samgoldman12@gmail.com>
2023-10-13 22:48:09 -0400

    a

--- packages/plugins/script/timer.ts
+++ packages/plugins/script/timer.ts
@@ -1,17 +1,47 @@
 import util from 'util'
 import { exec } from 'child_process'
 
+import ora from 'ora'
+import { deploy } from '../src/cli/deploy'
+
 const execAsync = util.promisify(exec)
+
+const spinner = ora()
 
 const main = async () => {
   let agg = 0
-  const runs = 20
+  const runs = 3
   for (let i = 0; i < runs; i++) {
+    spinner.start(`Starting run: ${i + 1} of ${runs}`)
+
+    exec(`anvil --silent &`)
+
     const begin = Date.now()
-    await execAsync('forge script script/Sphinx.s.sol')
+
+    const mockPrompt = async (_) => {}
+    await deploy(
+      './script/Sample.s.sol',
+      'anvil',
+      false, // Run preview
+      true, // Silent
+      undefined, // Only one contract in the script file, so there's no target contract to specify.
+      undefined, // Don't verify on Etherscan.
+      mockPrompt
+    )
+
+    // await execAsync(
+    //   'npx sphinx deploy script/Sample.s.sol --network anvil --confirm'
+    // )
+
     const end = Date.now()
     agg += (end - begin) / 1000
+
+    spinner.stop()
+    console.log('run', i + 1, (end - begin) / 1000)
+
+    await execAsync(`kill $(lsof -t -i:8545)`)
   }
+  spinner.stop()
   console.log('avg', (agg / runs).toFixed(3))
 }
 
