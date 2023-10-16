f61ecd5537c42b5801195316e82eccc27e71d2b7
Sam Goldman <samgoldman12@gmail.com>
2023-10-15 21:49:22 -0400

    a

--- packages/plugins/script/run-broadcast-tests.sh
+++ packages/plugins/script/run-broadcast-tests.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: unsilence
+
 # This file contains integration tests for broadcasting deployments onto forks of networks.
 # We silence all of the output to make the testing logs easier to read.
 
@@ -8,7 +10,7 @@
 set -e
 
 # Kill any existing anvil processes
-yarn test:kill > /dev/null
+yarn test:kill
 
 anvil --silent --port 42010 --fork-url https://opt-mainnet.g.alchemy.com/v2/$ALCHEMY_API_KEY &
 anvil --silent --port 42420 --fork-url https://opt-goerli.g.alchemy.com/v2/$ALCHEMY_API_KEY &
@@ -21,15 +23,15 @@ sleep 3
 # Deploy the `OnlyOptimism` contract to Optimism and Optimism Goerli. This is meant to be an
 # external contract, i.e. a contract that was previously deployed outside of Sphinx.
 onlyOptimismDeployedBytecode=$(forge inspect contracts/test/ChainSpecific.sol:OnlyOptimism deployedBytecode)
-cast rpc --rpc-url http://127.0.0.1:42010 anvil_setCode 0x0000000000000000000000000000000000000100 $onlyOptimismDeployedBytecode > /dev/null
-cast rpc --rpc-url http://127.0.0.1:42420 anvil_setCode 0x0000000000000000000000000000000000000100 $onlyOptimismDeployedBytecode > /dev/null
+cast rpc --rpc-url http://127.0.0.1:42010 anvil_setCode 0x0000000000000000000000000000000000000100 $onlyOptimismDeployedBytecode
+cast rpc --rpc-url http://127.0.0.1:42420 anvil_setCode 0x0000000000000000000000000000000000000100 $onlyOptimismDeployedBytecode
 
-npx sphinx deploy script/ChainSpecific.s.sol --network optimism --confirm --silent
-npx sphinx deploy script/ChainSpecific.s.sol --network optimism_goerli --confirm --silent
-npx sphinx deploy script/ChainSpecific.s.sol --network ethereum --confirm --silent
-npx sphinx deploy script/ChainSpecific.s.sol --network goerli --confirm --silent
-npx sphinx deploy script/ChainSpecific.s.sol --network arbitrum --confirm --silent
-npx sphinx deploy script/ChainSpecific.s.sol --network arbitrum_goerli --confirm --silent
+npx sphinx deploy script/ChainSpecific.s.sol --network optimism --confirm
+npx sphinx deploy script/ChainSpecific.s.sol --network optimism_goerli --confirm
+npx sphinx deploy script/ChainSpecific.s.sol --network ethereum --confirm
+npx sphinx deploy script/ChainSpecific.s.sol --network goerli --confirm
+npx sphinx deploy script/ChainSpecific.s.sol --network arbitrum --confirm
+npx sphinx deploy script/ChainSpecific.s.sol --network arbitrum_goerli --confirm
 forge test --match-contract Broadcast
 
-yarn test:kill > /dev/null
+yarn test:kill
