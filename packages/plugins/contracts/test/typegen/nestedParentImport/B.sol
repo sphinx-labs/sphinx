00941d2691d78e48d6adf5f312e989eceda7df8d
Ryan Pate <ryantpate97@gmail.com>
2023-10-19 13:55:13 -0400

    handle nested parent import

---
+++ packages/plugins/contracts/test/typegen/nestedParentImport/B.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./D.sol";
+import "./A.sol";
+
+contract B {
+    bool public boolean;
+
+    constructor (bool _boolean) {
+        boolean = _boolean;
+    }
+
+    function toggle() public {
+        boolean = !boolean;
+    }
+}
